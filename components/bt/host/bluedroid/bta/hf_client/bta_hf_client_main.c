3bb60d1fbba9ac5b0a25bb55de47545185f853f1
nif <nif@nifomorph.com>
2019-07-28 23:54:25 +0200

    TEMPORARY: Disable MSBC

--- components/bt/host/bluedroid/bta/hf_client/bta_hf_client_main.c
+++ components/bt/host/bluedroid/bta/hf_client/bta_hf_client_main.c
@@ -387,7 +387,7 @@ static void bta_hf_client_api_enable(tBTA_HF_CLIENT_DATA *p_data)
     bta_hf_client_cb.p_cback = p_data->api_enable.p_cback;
 
     /* check if mSBC support enabled */
-  #if 0
+  #if 1
     #warning "NIF/LIZN - Hack to avoid MSBC"
     // TODO: Deal differently with MSBC
     // bta_hf_client_cb.msbc_enabled = FALSE; // False already due to memset above
