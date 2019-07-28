27424073115585b52c189ed630a8f5784230d52c
nif <nif@nifomorph.com>
2019-07-29 00:27:59 +0200

    Utility to get string of SCO packet types from type mask.

---
+++ components/bt/host/bluedroid/stack/btm/btm_util_sco.c
@@ -0,0 +1,96 @@
+/**
+  * @author  nif / nifoMorph
+  * @brief   Utilities for mapping SCO flags into readable strings
+  * ------------------------------------------------------------------------
+  * Copyright nifoMorph ApS, 2019
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at:
+  *
+  * http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
+  * ------------------------------------------------------------------------
+ **/
+#include "stack/btm_util_sco.h"
+
+
+typedef struct tMask2StringMap* Mask2StringMapPtr;
+typedef const struct tMask2StringMap* Mask2StringMapCPtr;
+typedef struct tMask2StringMap
+{
+    long        mMask;
+    const char* cszString;
+} Mask2StringMapT;
+
+#define _S(X,VARIANT) #VARIANT #X
+#define _MMAP(X,VARIANT...) {.mMask = BTM_SCO_PKT_TYPE_MASK(X,VARIANT), .cszString = _S(X,VARIANT)}
+
+const Mask2StringMapT aPktTypeStringMap[] =
+{
+    _MMAP(EV5,3),
+    _MMAP(EV5,2),
+    _MMAP(EV3,3),
+    _MMAP(EV3,2),
+
+    _MMAP(EV5),
+    _MMAP(EV4),
+    _MMAP(EV3),
+
+    _MMAP(HV3),
+    _MMAP(HV2),
+    _MMAP(HV1),
+
+    {.mMask = ~0, .cszString = "X..."} // Catch all / end marker
+
+};
+
+
+UINT8 BTM_ScoPacketTypesToString (char* p_strbuf, UINT16 pkttype_mask)
+{
+  int nRes = 0;
+  char* pD = 0;
+  long  mMask = pkttype_mask;
+  Mask2StringMapCPtr pMap  = aPktTypeStringMap;
+
+  while(mMask)
+  {
+    if(mMask & pMap->mMask)
+    {
+      nRes++;
+      if(pD)
+      {
+        const char* pS = ",";
+        while(*pS) {*pD++ = *pS++;}
+      }
+      else
+      {
+        pD = p_strbuf;
+      }
+      if(pD)
+      {
+        const char* pS = pMap->cszString;
+        while(*pS) {*pD++ = *pS++;}
+      }
+      mMask &= ~pMap->mMask;
+    }
+    pMap++;
+  }
+  if(p_strbuf)
+  {
+    if(!pD)
+    {
+      pD = p_strbuf;
+      const char* pS = "NUL";
+      while(*pS) {*pD++ = *pS++;}
+    }
+    *pD = 0;
+  }
+  return nRes;
+}
---
+++ components/bt/host/bluedroid/stack/include/stack/btm_util_sco.h
@@ -0,0 +1,57 @@
+#pragma once
+/**
+  * @author  nif / nifoMorph
+  * @brief   Utilities for mapping SCO flags into readable strings
+  * ------------------------------------------------------------------------
+  * Copyright nifoMorph ApS, 2019
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at:
+  *
+  * http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
+  * ------------------------------------------------------------------------
+ **/
+#include "btm_api.h"
+
+
+#define BTM_SCO_PKT_TYPE_MASK_(KIND) (BTM_SCO_PKT_TYPES_MASK_##KIND)
+#define BTM_SCO_PKT_TYPE_MASK_2(KIND) (BTM_SCO_PKT_TYPES_MASK_NO_2_##KIND)
+#define BTM_SCO_PKT_TYPE_MASK_3(KIND) (BTM_SCO_PKT_TYPES_MASK_NO_3_##KIND)
+#define BTM_SCO_PKT_TYPE_MASK(KIND,VARIANT...) BTM_SCO_PKT_TYPE_MASK_##VARIANT(KIND)
+
+
+/*******************************************************************************
+**
+** Function         BTM_ScoPacketTypesToString
+**
+** Description      Map SCO packet type mask to a readable string.
+**
+** Parameters
+**   p_strbuf       Pointer to a buffer for the result.
+**                  Should be >= 80 bytes.
+**                  If NULL, no string is generated, but the number of packet
+**                  types matched are still returned.
+**
+**   pkttype_mask   Mask composed of bits from the definitions
+**                  BTM_SCO_PKT_TYPES_MASK_...
+**
+**                  If no bits are set, the string "NUL" is returned.
+**
+**                  If bits, which do not correspond to a known packet type,
+**                  are set, "X..." is added to the end of the string buffer
+**                  and the return value is incremented by one.
+**
+** Returns          Number of known packet types in the mask.
+**                  + 1 if one or more undefined bits are set.
+**
+*******************************************************************************/
+UINT8 BTM_ScoPacketTypesToString (char* p_strbuf, UINT16 pkttype_mask);
+
